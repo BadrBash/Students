d55109fe0ad42cf9d5b1c509d05fb80def4485a7
Mubashir Badr <148861725+BadrBash@users.noreply.github.com>
2024-04-09 10:01:49 +0100

    added departmentDTO

---
+++ DTOs/DepartmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeMVC.DTOs
+{
+    public class DepartmentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+    }
+}
