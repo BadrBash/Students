192d7dac8dbfc08e7f44a71db721556c5197def4
Mubashir Badr <148861725+BadrBash@users.noreply.github.com>
2024-04-09 10:07:48 +0100

    IDepartmentService added

---
+++ Services/Interface/IDepartmentService.cs
@@ -0,0 +1,14 @@
+﻿using PracticeMVC.Data.Entities;
+
+namespace PracticeMVC.Services.Interface
+{
+    public interface IDepartmentService
+    {
+        
+        List <Department> GetDepartmentById(int id);
+        public IEnumerable<Department> GetAllDepartments();
+        void CreateDepartment(Department department);
+        void UpdateDepartment(Department department);
+        void DeleteDepartment(int Id);
+    }
+}
